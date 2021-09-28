47c36c2a12d226c19213380e0690aea684a3a157
Pedro Appel <appelpedroappel@gmail.com>
2021-09-28 20:48:27 -0300

    Commit inicial

---
+++ js/register.js
@@ -0,0 +1,33 @@
+async function registerUser(){
+    if($("#txtPass").val() == $("#txtPass2").val()){
+        const form = {
+            "email": $("#txtEmail").val(),
+            "name": $("#txtName").val(),
+            "password": $("#txtPass").val(),
+            "surname": $("#txtSurname").val(),
+            "phone": $("#txtPhone").val(),
+            "address": $("#txtAddress").val(),
+            "documentNumber": $("#txtDocumentNumber").val()
+        }
+        fetch("http://localhost:8080/user/create", {
+            method: "POST", 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form)
+          }).then(res => {
+            console.log("Request complete! response:", res);
+          });
+    }else {
+        console.log("VOCE ERROU FILHO DA PUTA")
+    }
+}
+
+async function getUser(){
+    console.log(document.register.elements[2]);
+    const user_id = "ae5b817e-e839-428b-93b2-e47d4bb97fb6";
+    const response = await fetch(`http://localhost:8080/snkr/${user_id}`);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.content[0].price)
+}
---
+++ js/tab.js
@@ -0,0 +1,22 @@
+function abreAba(evt, nomeAba) {
+  
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(nomeAba).style.display = "block";
+
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
